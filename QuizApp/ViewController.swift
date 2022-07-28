f6f29dc0922b0212a24d53a5b1955528fe7bf51e
HarpritKaurSidhu <35619714+HarpritKaurSidhu@users.noreply.github.com>
2022-07-28 18:57:38 -0400

    Created UI of welcome and signup screens

--- QuizApp/ViewController.swift
+++ QuizApp/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onStartButtonTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "goToWelcomeScreen", sender: self)
+    }
+    
 }
 
---
+++ QuizApp/WelcomeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  WelcomeViewController.swift
+//  QuizApp
+//
+//  Created by Harprit on 2022-07-27.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSignUpButtonTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "goToSignUpScreen", sender: self)
+    }
+    
+    @IBAction func onLoginButtonTapped(_ sender: UIButton) {
+    
+        performSegue(withIdentifier: "goToLoginScreen", sender: self)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
