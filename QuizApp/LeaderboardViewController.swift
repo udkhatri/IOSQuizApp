fedcdcb5fef9a4dd0002765030c54e08aa00c8ba
HarpritKaurSidhu <35619714+HarpritKaurSidhu@users.noreply.github.com>
2022-08-10 13:24:14 -0400

    tableview for quiz answers and leaders done

--- QuizApp/LeaderboardViewController.swift
+++ QuizApp/LeaderboardViewController.swift
@@ -7,26 +7,47 @@
 
 import UIKit
 
-class LeaderboardViewController: UIViewController {
+class LeaderboardViewController: UIViewController, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    private var items: [ItemToDo] = []
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "leaderCell", for: indexPath)
+        let item = items[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = item.title
+        content.secondaryText =  item.description
+        cell.contentConfiguration = content
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        loadDefaultItems()
+        tableView.dataSource = self
     }
     
 
     @IBAction func onBackButtonTapped(_ sender: UIButton) {
         dismiss(animated: true)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func loadDefaultItems(){
+        items.append(ItemToDo(title: "title 1", description: "Description 1"))
+        items.append(ItemToDo(title: "title 2", description: "Description 2"))
+        items.append(ItemToDo(title: "title 3", description: "Description 3"))
     }
-    */
+
+    struct ItemToDo{
+        let title: String
+        let description: String
+    }
 
 }
--- QuizApp/QuestionViewController.swift
+++ QuizApp/QuestionViewController.swift
@@ -7,26 +7,50 @@
 
 import UIKit
 
-class QuestionViewController: UIViewController {
+class QuestionViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
 
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "toDoCell", for: indexPath)
+        let item = items[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = item.title
+        content.secondaryText =  item.description
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+
+    
+
+    @IBOutlet weak var tableView: UITableView!
+
+    private var items: [ItemToDo] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        loadDefaultItems()
+        tableView.dataSource = self
     }
     
 
     @IBAction func onQuitButtonTapped(_ sender: UIButton) {
         performSegue(withIdentifier: "goToSelectCategoryScreen", sender: self)
     }
-    /*
-    // MARK: - Navigation
+    private func loadDefaultItems(){
+        items.append(ItemToDo(title: "title 1", description: "Description 1"))
+        items.append(ItemToDo(title: "title 2", description: "Description 2"))
+        items.append(ItemToDo(title: "title 3", description: "Description 3"))
+        items.append(ItemToDo(title: "title 4", description: "Description 4"))
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
-
 }
+    struct ItemToDo{
+        let title: String
+        let description: String
+    }
+
